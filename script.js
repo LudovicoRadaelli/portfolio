7f90d95bd873ba1bab8a26b59cdfcf4d40b1f6cf
LudovicoRadaelli <69393267+LudovicoRadaelli@users.noreply.github.com>
2022-11-06 12:35:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function goToLink(n) {
+    if(n === 0) {location.href = "https://ludovicoradaelli.github.io/Mat/1/scomposizionePolinomi/index.html#esempio3-title";}
+    if(n === 1) {location.href = "https://ludovicoradaelli.github.io/Mat/1/frazioniEquivalenti/index.html";}
+    if(n === 2) {location.href = "https://ludovicoradaelli.github.io/Mat/4/4Bs/goniometria/4#tool";}
+    if(n === 3) {location.href = "https://ludovicoradaelli.github.io/creativeCoding/pinholeCamera/index.html";}
+    if(n === 4) {location.href = "https://ludovicoradaelli.github.io/creativeCoding/squareSpiral/index.html";}
+}
